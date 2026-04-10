6dc66ced0fd930ca090ef8e8da70651ee580ea51
Rahin Haque <haquerahin743@gmail.com>
2026-04-10 08:01:36 +0600

    finding if the number is even or odd

---
+++ evenOrOdd.js
@@ -0,0 +1,6 @@
+// Write a function that tells if a number is even or odd
+function evenOdd(a){
+ return a % 2 === 0 ? "The number is even" : "The number is odd"
+}
+console.log(evenOdd(7));
+console.log(evenOdd(-4));;
