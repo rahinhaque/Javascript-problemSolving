0a779d86d2b74f0c755a48ebbeb225b89759544a
Rahin Haque <haquerahin743@gmail.com>
2026-04-13 07:36:56 +0600

    function that accepts a parameter representing a name and returns the message

--- problem#.js
+++ problem#.js
@@ -1,11 +1,12 @@
 // Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 
 function getSum(a, b) {
- let start = Math.max(a , b);
- let sum = 0;
-  for (i = a; i <= b; i++) {
-   sum = sum + i;
+  let sum = 0;
+  let start = Math.min(a, b);
+  let end = Math.max(a, b);
+  for (i = start; i <= end; i++) {
+    sum = sum + i;
   }
   return sum;
 }
-console.log(getSum(-1 , 0));
+console.log(getSum(-1, 2));
